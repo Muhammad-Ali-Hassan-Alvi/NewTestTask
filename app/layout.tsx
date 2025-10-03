5f1e14d9d9616f428f5754550fffbb0a50d36e6c
Muhammad Ali <muhammadalialvi646@gmail.com>
2025-10-03 20:29:13 +0500

    Changes related to the filteration of names

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Test Task Scheduler',
+  description: 'This is a test task scheduler',
   generator: 'v0.app',
 }
 
